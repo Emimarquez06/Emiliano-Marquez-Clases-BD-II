b715ece77d9491c88195d4958c1d513771cd1fda
Emimarquez06 <106921961+Emimarquez06@users.noreply.github.com>
2025-11-29 10:34:23 -0300

    Update Clase 11.sql

--- Clases/Clase 11.sql
+++ Clases/Clase 11.sql
@@ -1,65 +1,79 @@
-#Carlos Vercellone Clase 9
-use sakila;
+USE sakila;
 
-select f.title as films_not_in_Inventory from film f
-where f.film_id not in ( 
+-- 1) 
+SELECT 
+    f.title
+FROM film f
+LEFT JOIN inventory i 
+    ON f.film_id = i.film_id
+WHERE i.inventory_id IS NULL;
 
-select distinct i.film_id from inventory i
+-- 2)
+SELECT DISTINCT 
+    f.title,
+    i.inventory_id
+FROM inventory i
+LEFT JOIN rental r 
+    ON i.inventory_id = r.inventory_id
+JOIN film f 
+    ON f.film_id = i.film_id
+WHERE r.rental_id IS NULL;
 
-);
+-- 3) 
+SELECT 
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    s.store_id,
+    f.title,
+    r.rental_date,
+    r.return_date
+FROM customer c
+JOIN rental r 
+    ON c.customer_id = r.customer_id
+JOIN inventory i 
+    ON r.inventory_id = i.inventory_id
+JOIN film f 
+    ON i.film_id = f.film_id
+JOIN store s 
+    ON s.store_id = i.store_id
+ORDER BY s.store_id, c.last_name;
 
+-- 4)
+SELECT 
+    s.store_id,
+    SUM(p.amount) AS total_sales
+FROM store s
+JOIN staff st 
+    ON s.store_id = st.store_id
+JOIN payment p 
+    ON st.staff_id = p.staff_id
+GROUP BY s.store_id;
 
-select f.title, i.inventory_id from film f
-inner join inventory i on i.film_id = f.film_id
-where i.inventory_id not in (
+-- 5)
+SELECT 
+    s.store_id,
+    CONCAT(ct.city, ', ', co.country) AS location,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager,
+    SUM(p.amount) AS total_sales
+FROM store s
+JOIN address a 
+    ON s.address_id = a.address_id
+JOIN city ct 
+    ON a.city_id = ct.city_id
+JOIN country co 
+    ON ct.country_id = co.country_id
+JOIN staff m 
+    ON s.manager_staff_id = m.staff_id
+JOIN payment p 
+    ON p.staff_id = m.staff_id
+GROUP BY s.store_id, location, manager;
 
-select r.inventory_id from rental r
-
-);
-
-
-select c.first_name, c.last_name, c.store_id, 
-f.title, r.rental_date, r.return_date from customer c
-inner join rental r on r.customer_id = c.customer_id
-inner join inventory i on r.inventory_id = i.inventory_id
-inner join film f on i.film_id = f.film_id
-order by c.store_id, c.last_name;
-
-
-
-select concat(ct.city, ", ", co.country) as location,
-concat(st.first_name, " ", st.last_name) as manager,
-(
-
-select sum(p.amount) from payment p
-inner join rental r on r.rental_id = p.rental_id
-inner join customer c on c.customer_id = r.customer_id
-where c.store_id = s.store_id
-
-) as total_sales
-from store s
-inner join staff st on s.manager_staff_id = st.staff_id
-inner join address a on a.address_id = s.address_id
-inner join city ct on a.city_id = ct.city_id
-inner join country co on co.country_id = ct.country_id;
-
-
-
--- Which actor has appeared in the most films?
-
-select 
-a.first_name, 
-a.last_name,
-(
-
-select count(*) from film f 
-inner join film_actor fa on f.film_id = fa.film_id
-where fa.actor_id = a.actor_id
-
-) as total_apearences
-from actor a
-order by total_apearences desc
-limit 1;
-
-
-
+-- 6)
+SELECT 
+    CONCAT(a.first_name, ' ', a.last_name) AS actor,
+    COUNT(fa.film_id) AS total_movies
+FROM actor a
+JOIN film_actor fa 
+    ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id
+ORDER BY total_movies DESC
+LIMIT 1;
